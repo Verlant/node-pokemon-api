965155149bd4aef9a175e39da1133138d426668e
Antoine Verlyck <antoine.verlyck@gmail.com>
2023-10-08 14:22:49 +0200

    update

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello, Node ! 👋")
+const express = require('express')
+let pokemons = require("./mock-pokemon")
+
+const app = express()
+const port = 3000
+app.get('/', (req, res) => res.send('Hello again, Express ! 👋'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}.`)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`)
+})
+
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
